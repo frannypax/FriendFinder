e29bc1445c944c87297edcb08b2f02bf7ad56f0c
yaw owusu <asirifiyaw@gmail.com>
2017-11-02 15:30:33 -0700

    Created friends array, added instructions file

---
+++ app/data/friends.js
@@ -0,0 +1,41 @@
+var friends = [
+	{
+		"name": "Frank",
+		"photo": "https://www.google.com/search?q=men+pictures&source=lnms&tbm=isch&sa=X&ved=0ahUKEwi5mai29qDXAhVh5oMKHYAEA44Q_AUICigB&biw=1366&bih=662#imgrc=Va1leSTvN5AiqM:",
+		"scores":[
+			5,4,3,2,1,5,4,3,2,1
+		]
+	},
+	{
+		"name": "Danny",
+		"photo": "https://www.google.com/search?q=men+pictures&source=lnms&tbm=isch&sa=X&ved=0ahUKEwi5mai29qDXAhVh5oMKHYAEA44Q_AUICigB&biw=1366&bih=662#imgrc=J7sbHzE0bnVi5M:",
+		"scores":[
+			1,2,3,4,5,1,2,3,4,5
+		]
+	},
+	{
+		"name": "Kate",
+		"photo": "https://www.google.com/search?q=men+pictures&source=lnms&tbm=isch&sa=X&ved=0ahUKEwi5mai29qDXAhVh5oMKHYAEA44Q_AUICigB&biw=1366&bih=662#imgdii=_Drdj_vUKdXytM:&imgrc=J7sbHzE0bnVi5M:",
+		"scores":[
+			5,5,4,4,1,5,5,4,4,1
+		]
+	},
+	{
+		"name": "Vic",
+		"photo": "https://www.google.com/search?q=men+pictures&source=lnms&tbm=isch&sa=X&ved=0ahUKEwi5mai29qDXAhVh5oMKHYAEA44Q_AUICigB&biw=1366&bih=662#imgdii=dclkbTNHpcKGIM:&imgrc=J7sbHzE0bnVi5M:",
+		"scores":[
+			4,4,3,3,2,4,4,3,3,2
+		]
+	},
+	{
+		"name": "Mike",
+		"photo": "https://www.google.com/search?q=men+pictures&source=lnms&tbm=isch&sa=X&ved=0ahUKEwi5mai29qDXAhVh5oMKHYAEA44Q_AUICigB&biw=1366&bih=662#imgrc=iZaM2L-55_tcDM:",
+		"scores":[
+			3,3,3,3,3,3,3,3,3,3
+		]
+	},
+
+]
+
+//Export Array
+module.exports = friends;
